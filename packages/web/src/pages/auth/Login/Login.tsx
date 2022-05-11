15d3bdb5adbe496e9a8ff5d65c0525ba459869c2
Nataliia Sychevska <petriwna43@gmail.com>
2022-05-11 18:30:41 +0300

    add login

--- packages/web/src/pages/auth/Login/Login.tsx
+++ packages/web/src/pages/auth/Login/Login.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import * as React from 'react';
 import { useFormik } from 'formik';
-import axios from 'axios';
-
 import * as yup from 'yup';
+import axios from 'axios';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
@@ -14,12 +14,12 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 
+import { NavLink } from '../../shared-components';
 import { Alert } from '../components/Alert';
 import { Form } from '../components/Form';
-import { NavLink } from '../../shared-components';
 
 const validationSchema = yup.object({
-  usernameOrEmail: yup.string().email('Enter a valid email').required('Email is required'),
+  email: yup.string().email('Enter a valid email').required('Email is required'),
   password: yup.string().min(8, 'Password should be of minimum 8 characters length').required('Password is required'),
 });
 
@@ -34,16 +34,15 @@ export default function Login() {
       formikHelpers.setSubmitting(false);
 
       try {
-        console.log(values);
         const url = 'http://localhost:3500/authentication/log-in';
         const { data } = await axios.post(url, values, { withCredentials: true });
-        console.log(`Is Login ${data}`);
-        console.log(data);
+        console.log('authorization');
       } catch (e) {
-        console.log(e.response.data.message, 'error');
+        console.log(e.response.data.message);
         // @ts-ignore
         formikHelpers.setErrors({ errorMsg: e.response.data.message });
       }
+
       // onSubmit(values).catch((e) => {
       //   formikHelpers.setSubmitting(false);
       //   if (isFormError(e)) {
@@ -57,6 +56,7 @@ export default function Login() {
 
   return (
     <Container component="main" maxWidth="xs">
+      <CssBaseline />
       <Box
         sx={{
           marginTop: 8,
@@ -69,19 +69,19 @@ export default function Login() {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          SignIn
+          Sign in
         </Typography>
-        <Form name="log-in" onSubmit={formik.handleSubmit} sx={{ mt: 1 }}>
+        <Form name="login" onSubmit={formik.handleSubmit} noValidate sx={{ mt: 1 }}>
           <Box sx={{ marginBottom: 2 }}>
             {/* @ts-ignore */}
-            <Alert color="error">{formik.errors.errorMsg}</Alert>
+            {formik.errors && formik.errors?.errorMsg && <Alert color="error">{formik.errors.errorMsg}</Alert>}
           </Box>
           <TextField
             margin="normal"
             required
             fullWidth
             id="email"
-            label="Email"
+            label="Email Address"
             name="email"
             autoComplete="email"
             autoFocus
@@ -106,7 +106,7 @@ export default function Login() {
           />
           <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
           <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }} disabled={formik.isSubmitting}>
-            Submit
+            Sign In
           </Button>
           <Grid container>
             <Grid item xs>
@@ -116,7 +116,7 @@ export default function Login() {
             </Grid>
             <Grid item>
               <NavLink to="/signup" variant="body2">
-                Don&apos;t have an account? Sign Up
+                Do not have an account? Sign Up
               </NavLink>
             </Grid>
           </Grid>
